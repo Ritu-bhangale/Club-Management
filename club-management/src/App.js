8e977fdd1d54f120eac3e4b30d7b83a29d022ea6
Ritu Bhangale <bt20cse021@iiitn.ac.in>
2022-01-20 19:09:35 +0530

    update

--- club-management/src/App.js
+++ club-management/src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Landing page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, architecto?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, architecto?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, architecto?</p>
     </div>
   );
 }
