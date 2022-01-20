eb8cee6b87994617129386f686ca7ce8f79c5b15
Ritu Bhangale <bt20cse021@iiitn.ac.in>
2022-01-21 02:36:00 +0530

    Design System

--- club-management/src/pages/login/login.js
+++ club-management/src/pages/login/login.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from '../../components/button/button'
 
 function Login() {
     return (
         <>
         <h1>Login</h1>
+        <Button buttonStyle="btn-normal">events</Button>
+        <Button buttonStyle="btn-line">Events</Button>
+        <Button buttonStyle="btn-border">Events</Button>
         </>
     );
 }
