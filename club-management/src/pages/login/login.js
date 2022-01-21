cd91705aa86a5e7230a76510aafcd560ae654d19
Shrishti Shah <shrishtishah2002@gmail.com>
2022-01-21 16:10:52 +0530

    trials

--- club-management/src/pages/login/login.js
+++ club-management/src/pages/login/login.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Button from '../../components/button/button'
 
 function Login() {
-    return (
-        <>
-        <h1>Login</h1>
-        <Button buttonStyle="btn-normal">events</Button>
-        <Button buttonStyle="btn-line">Events</Button>
-        <Button buttonStyle="btn-border">Events</Button>
-        </>
+    return ( <
+        >
+        <
+        h1 div = "head" > Login < /h1> <
+        Button buttonStyle = "btn-normal" > events < /Button> <
+        Button buttonStyle = "btn-line" > Events < /Button> <
+        Button buttonStyle = "btn-border" > Events < /Button> <
+        />
     );
 }
 
