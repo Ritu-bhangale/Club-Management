f8c5959bcd3328025ccffd662c9e6804a8b78bb3
Shrishti Shah <shrishtishah2002@gmail.com>
2022-01-31 23:44:32 +0530

    schemA fors chema

---
+++ server/models/clubModel.js
@@ -0,0 +1,37 @@
+const { type } = require("express/lib/response")
+const mongoose = require("mongoose")
+const clubSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Enter your Club Name"]
+    },
+    wingname: {
+        type: String,
+        required: [true, "Enter name og the wing under which your club lies"]
+    },
+    website: {
+        type: URL,
+        required: [true, "Enter your website link"]
+    },
+    description: {
+        type: String,
+        required: [true, "Write about the club in 200 characters"],
+        maxlength: [200, "About us cannot exceed 200 characters"]
+    },
+    images: [{
+        public_id: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        }
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+module.exports = mongoose.model("Clubs", clubSchema)
