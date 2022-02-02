6663ed457879a80e5421f58a06bb167eb7fa91d9
Shrishti Shah <shrishtishah2002@gmail.com>
2022-02-02 23:59:09 +0530

    just added form pages

--- club-management/src/App.js
+++ club-management/src/App.js
@@ -3,7 +3,8 @@ import Register from './pages/register/register';
 import ClubDetails from './pages/club-detail/clubDetail';
 import Calendar from './pages/Calendar/calendar'
 import Login from './pages/login/login';
-import Home from './pages/home/home'
+import Home from './pages/home/home';
+import ClubDetail_form from './pages/clubDetail_Form';
 import {
   BrowserRouter as Router,
   Switch,
@@ -22,6 +23,7 @@ function App() {
         <Route path="/login" element={<Login/>}/>
         <Route path="/clubdetails" element={<ClubDetails/>}/>
         <Route path="/calendar" element={<Calendar/>}/>
+        <Route path="/clubForm" element={<ClubDetail_form/>}/>
       </Routes>
     </Router>
     </>
---
+++ club-management/src/pages/clubDetail_Form/clubDetail_form.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const clubDetail_form = () => {
+  return <div></div>;
+};
+
+export default clubDetail_form;
